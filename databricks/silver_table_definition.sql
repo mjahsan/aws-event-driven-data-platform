9c3e69e20ba2936d4ebba01c942fe9107e75201f
Mohamed Ahsan <inboxofahsan@gmail.com>
2026-02-21 06:57:50 +0000

    Updating the Databrick's DDL

--- databricks/silver_table_definition.sql
+++ databricks/silver_table_definition.sql
@@ -4,52 +4,68 @@ USE demo_catalog;
 
 CREATE SCHEMA IF NOT EXISTS demo_catalog.silver;
 
+DROP TABLE IF EXISTS silver.events_user;
+
 CREATE TABLE IF NOT EXISTS silver.events_user (
   file_id STRING,
   domain STRING,
   source_system STRING,
   created_at TIMESTAMP,
-  event_id STRING PRIMARY KEY, #Primary key is only for decorative purpose and optional as delta does not enforce it 
+  event_id STRING, 
   event_type STRING, 
   source STRING,
   event_ts TIMESTAMP,
   ingest_ts TIMESTAMP,
+  event_date DATE,
   user_id STRING,
   email STRING,
   country STRING,
   device STRING
-  );
+)
+USING DELTA
+PARTITIONED BY (event_date)
+;
 
-  CREATE TABLE IF NOT EXISTS silver.events_payment (
+DROP TABLE IF EXISTS silver.events_payment;
+
+CREATE TABLE IF NOT EXISTS silver.events_payment(
   file_id STRING,
   domain STRING,
   source_system STRING,
   created_at TIMESTAMP,
-  event_id STRING PRIMARY KEY, #Primary key is only for decorative purpose and optional as delta does not enforce it 
+  event_id STRING,
   event_type STRING, 
   source STRING,
   event_ts TIMESTAMP,
   ingest_ts TIMESTAMP,
+  event_date DATE,
   payment_id STRING,
   order_id STRING,
   amount DECIMAL(10,2),
   currency STRING,
   failure_reason STRING
-  );
+)
+USING DELTA
+PARTITIONED BY (event_date);
 
-  CREATE TABLE IF NOT EXISTS silver.events_order (
+DROP TABLE IF EXISTS silver.events_order;
+
+CREATE TABLE IF NOT EXISTS silver.events_order (
   file_id STRING,
   domain STRING,
   source_system STRING,
   created_at TIMESTAMP,
-  event_id STRING PRIMARY KEY, #Primary key is only for decorative purpose and optional as delta does not enforce it 
+  event_id STRING,  
   event_type STRING, 
   source STRING,
   event_ts TIMESTAMP,
   ingest_ts TIMESTAMP,
+  event_date DATE,
   order_id STRING,
   user_id STRING,
   amount DECIMAL(10,2),
   currency STRING,
   status STRING
-  );
+)
+USING DELTA
+PARTITIONED BY (event_date);
