d1b3ca61101e9417de3b07de48d38d367d044d3e
Mohamed Ahsan <inboxofahsan@gmail.com>
2026-02-24 16:29:02 +0000

    Updating maintenance job commands

---
+++ databricks/maintenance_job.sql
@@ -0,0 +1,9 @@
+-- Cell 1: Optimize and Z-Order
+OPTIMIZE demo_catalog.silver.events_user ZORDER BY (event_id);
+OPTIMIZE demo_catalog.silver.events_payment ZORDER BY (event_id, order_id);
+OPTIMIZE demo_catalog.silver.events_order ZORDER BY (event_id, order_id);
+
+-- Cell 2: Clean up old file versions (Keep last 7 days)
+VACUUM demo_catalog.silver.events_user RETAIN 168 HOURS;
+VACUUM demo_catalog.silver.events_payment RETAIN 168 HOURS;
+VACUUM demo_catalog.silver.events_order RETAIN 168 HOURS;
