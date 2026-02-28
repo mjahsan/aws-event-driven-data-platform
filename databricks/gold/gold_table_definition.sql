01e940a034b07f5d4d63cd1a602a2338bf2895cd
Mohamed Ahsan <inboxofahsan@gmail.com>
2026-02-28 19:37:53 +0000

    Databrick Gold DDL

--- databricks/gold/gold_table_definition.sql
+++ databricks/gold/gold_table_definition.sql
@@ -4,6 +4,7 @@ URL 's3://event-platform-new/silver'
 WITH (CREDENTIAL `aws-databricks-connection-role`);
 -- Note that this 'silver' external location already exist and this role already has access to create table, read and write files in silver location which was granted during silver layer DDL.
 -- If access restriction is needed for gold layer, a new IAM role and the carrying out the below steps would be required.
+-- Safe to remove if already done in silver layer DDL
 
 CREATE EXTERNAL LOCATION s3_gold_location
 URL 's3://event-platform-new/gold'
@@ -20,7 +21,8 @@ GRANT USE CATALOG ON CATALOG demo_catalog TO `your-databricks-user`;
 
 -- Granting access to silver (read access for silver data)
 GRANT READ FILES ON EXTERNAL LOCATION s3_silver_location TO `your-databricks-user`;
--- Note that the above grant has no effect as the location and IAM already have access to read and write granted during silver layer DDL.
+-- Note that the above grant on 'silver' has no effect as the location and IAM already have access to read and write granted during silver layer DDL.
+-- Safe to remove if already done in silver layer DDL
 
 -- Granting access to silver (read and write for transformed data)
 GRANT USE SCHEMA, CREATE TABLE ON SCHEMA demo_catalog.gold TO `your-databricks-user`;
@@ -41,7 +43,7 @@ CREATE TABLE IF NOT EXISTS gold.fact_orders(
 )
 USING DELTA
 PARTITIONED BY (order_date)
-LOCATION 's3://event-platform-new/gold/fact_orders'
+LOCATION 's3://event-platform-new/gold/fact_orders';
 
 DROP TABLE IF EXISTS gold.fact_payments;
 CREATE TABLE IF NOT EXISTS gold.fact_payments(
@@ -54,7 +56,7 @@ CREATE TABLE IF NOT EXISTS gold.fact_payments(
 )
 USING DELTA
 PARTITIONED BY (payment_date)
-LOCATION 's3://event-platform-new/gold/fact_payments'
+LOCATION 's3://event-platform-new/gold/fact_payments';
 
 DROP TABLE IF EXISTS gold.dim_users;
 CREATE TABLE IF NOT EXISTS gold.dim_users(
@@ -63,7 +65,7 @@ CREATE TABLE IF NOT EXISTS gold.dim_users(
   country STRING
 )
 USING DELTA
-LOCATION 's3://event-platform-new/gold/dim_users'
+LOCATION 's3://event-platform-new/gold/dim_users';
 
 DROP TABLE IF EXISTS gold.dim_devices;
 CREATE TABLE IF NOT EXISTS gold.dim_devices(
@@ -71,4 +73,4 @@ CREATE TABLE IF NOT EXISTS gold.dim_devices(
   device_type STRING
 )
 USING DELTA
-LOCATION 's3://event-platform-new/gold/dim_devices'
+LOCATION 's3://event-platform-new/gold/dim_devices';
